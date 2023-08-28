24c9d053fe784b79185b53cb082cb1ed66309015
yongseok kim <yongseok@saerom.co.kr>
2023-08-28 16:16:53 +0900

    TailwindCSS setting

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// tailwind를 일반 css 파일로 빌드하기 위해 필요
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind를 커스터마이즈하기 위해 필요
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
