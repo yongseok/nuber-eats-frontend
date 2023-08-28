534941f7b6a513572ea6a3b255f45d9d0c96b676
yongseok kim <yongseok@saerom.co.kr>
2023-08-28 23:57:33 +0900

    useMutation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { gql } from '../__generated__';
+import { useMutation } from '@apollo/client';
+
+const LOGIN_MUTAION = gql(`
+  mutation banana($input: LoginInput!) {
+    login(input: $input) {
+      ok
+      error
+      token
+    }
+  }
+`);
 
 interface ILoginForm {
-  email?: string;
-  password?: string;
+  email: string;
+  password: string;
 }
 
 export const Login = () => {
@@ -12,7 +24,20 @@ export const Login = () => {
     formState: { errors },
     handleSubmit,
   } = useForm<ILoginForm>();
-  const onSubmit = () => {};
+
+  const [loginMutation, { data: resultData }] = useMutation(LOGIN_MUTAION);
+
+  const onSubmit = (data: ILoginForm) => {
+    const { email, password } = data;
+    loginMutation({
+      variables: {
+        input: {
+          email,
+          password,
+        },
+      },
+    });
+  };
   return (
     <div className='h-screen flex items-center justify-center bg-gray-800'>
       <div className='bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center'>
@@ -36,7 +61,7 @@ export const Login = () => {
           <input
             {...register('password', {
               required: 'Password is required',
-              minLength: 10,
+              minLength: 4,
             })}
             required
             type='password'
@@ -50,10 +75,15 @@ export const Login = () => {
           )}
           {errors.password?.type === 'minLength' && (
             <span className='font-medium text-red-500'>
-              Password must be more than 10 chars.
+              Password must be more than 4 chars.
             </span>
           )}
           <button className='mt-3 btn'>Log In</button>
+          {resultData?.login?.error && (
+            <span className='font-medium text-red-500'>
+              {resultData?.login?.error}
+            </span>
+          )}
         </form>
       </div>
     </div>
